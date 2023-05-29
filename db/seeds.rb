1766351931db3cbae0c8e456979914636dc4a8a6
eugene dami <eugene.dami@yahoo.fr>
2023-05-29 17:54:28 +0200

    Done every views, done controller, done routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,37 @@
-User.create
+bono = User.create!(
+  first_name: 'Bono',
+  last_name: 'Bo',
+  email: 'bonobo@gmail.com',
+  password: 'azerty',
+  password_confirmation: 'azerty'
+)
 
 Bonzai.create!(
-  name: 'Banzai!',
+  user: bono,
+  name: 'test',
   address: '10 Clifton Gardens London W9 1DT',
-  description: 'Beware! it bites',
+  description: 'blablabla',
   price_per_day: 75,
   species: 'Pokemon plant type',
   photo_url: 'https://www.123-stickers.com/7670-large_default/autocollant-bulbizarre-pokemon-001.jpg'
 )
 
-Bonzai.create!(
-  name: 'Nabillai',
-  address: '10 Clifton Gardens Paris W9 1DT',
-  description: 'Non mais allo quoi',
-  price_per_day: 12,
-  species: 'Pokemon boring type',
-  photo_url: 'https://kalima-ich.e-monsite.com/medias/images/img-1569.jpg'
-)
+# Bonzai.create!(
+#   user: bob,
+#   name: 'Nabillai',
+#   address: '10 Clifton Gardens Paris W9 1DT',
+#   description: 'Non mais allo quoi',
+#   price_per_day: 12,
+#   species: 'Pokemon boring type',
+#   photo_url: 'https://kalima-ich.e-monsite.com/medias/images/img-1569.jpg'
+# )
 
-Bonzai.create!(
-  name: 'Pasbonzai',
-  address: '10 Clifton Gardens Pekin W9 1DT',
-  description: 'Il est pas bon',
-  price_per_day: 120,
-  species: 'Pokemon disgusting type',
-  photo_url: 'https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2021/01/13004818/Chocolate-Bonsai.jpg'
-)
+# Bonzai.create!(
+#   user: bella,
+#   name: 'Pasbonzai',
+#   address: '10 Clifton Gardens Pekin W9 1DT',
+#   description: 'Il est pas bon',
+#   price_per_day: 120,
+#   species: 'Pokemon disgusting type',
+#   photo_url: 'https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2021/01/13004818/Chocolate-Bonsai.jpg'
+# )
