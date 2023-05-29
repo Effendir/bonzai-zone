d1d86f92fa2763bdf60f148d13b981020a38a44a
eugene dami <eugene.dami@yahoo.fr>
2023-05-29 16:39:35 +0200

    done every view except show

--- app/controllers/bonzais_controller.rb
+++ app/controllers/bonzais_controller.rb
@@ -1,25 +1,50 @@
 class BonzaisController < ApplicationController
+  before_action :set_bonzai, only: [:show, :edit, :update, :destroy]
+
   def index
     @bonzais = Bonzai.all
   end
 
   def show
-
+    @bonzai =  Bonzai.find(params[:id])
   end
 
   def new
-   
+    @bonzai = Bonzai.new
   end
 
   def create
+    @bonzai = Bonzai.new(bonzai_params)
+    if @bonzai.save
+      redirect_to bonzais_path(@bonzai)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @bonzai.update(bonzai_params)
+      redirect_to bonzai_path(@bonzai)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @bonzai.destroy
+    redirect_to bonzais_path, status: :see_other
+  end
+
+  private
+
+  def set_bonzai
+    @bonzai = Bonzai.find(params[:id])
+  end
+
+  def bonzai_params
+    params.require(:bonzai).permit(:name, :species, :photo_url, :description, :address, :price_per_day)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create
+
+Bonzai.create!(
+  name: 'Banzai!',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Beware! it bites',
+  price_per_day: 75,
+  species: 'Pokemon plant type',
+  photo_url: 'https://www.123-stickers.com/7670-large_default/autocollant-bulbizarre-pokemon-001.jpg'
+)
+
+Bonzai.create!(
+  name: 'Nabillai',
+  address: '10 Clifton Gardens Paris W9 1DT',
+  description: 'Non mais allo quoi',
+  price_per_day: 12,
+  species: 'Pokemon boring type',
+  photo_url: 'https://kalima-ich.e-monsite.com/medias/images/img-1569.jpg'
+)
+
+Bonzai.create!(
+  name: 'Pasbonzai',
+  address: '10 Clifton Gardens Pekin W9 1DT',
+  description: 'Il est pas bon',
+  price_per_day: 120,
+  species: 'Pokemon disgusting type',
+  photo_url: 'https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2021/01/13004818/Chocolate-Bonsai.jpg'
+)
