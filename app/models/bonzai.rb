44f8f51cdb0ce3480f6dc5a9eda452331a4d1517
Helene Becquembois <hbecquembois@gmail.com>
2023-05-30 14:45:39 +0200

    added associations and validations in User and Bonzai models

--- app/models/bonzai.rb
+++ app/models/bonzai.rb
@@ -1,3 +1,10 @@
 class Bonzai < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_day, format: { with: /\A\d+(\.\d{1,2})?\z/ }
+  validates :price_per_day, numericality: { greater_than: 0 }
 end
