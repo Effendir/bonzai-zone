e15c7fd1d707d31682df79aadb5153ed8c8ac626
eugene dami <eugene.dami@yahoo.fr>
2023-05-31 15:06:29 +0200

    done dashboard with link on profile pic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
 
   def show
     @user = current_user
+    @user_bonzais = Bonzai.where(user_id: @user.id)
+    @user_bookings = Booking.where(user_id: @user.id)
     @bookings = Booking.all
+    @requests = @bookings.select do |booking|
+      booking.bonzai.user == @user
+    end
   end
 end
