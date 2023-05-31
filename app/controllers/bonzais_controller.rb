11383353731aa024f96a138ef33927210eabe4eb
Lucie Gaget <lucie-gaget@orange.fr>
2023-05-31 11:58:24 +0200

    login home

--- app/controllers/bonzais_controller.rb
+++ app/controllers/bonzais_controller.rb
@@ -1,4 +1,5 @@
 class BonzaisController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_bonzai, only: [:show, :edit, :update, :destroy]
 
   def index
